42a04b8230a22bc613aeaf2eabce636b5904ee2e
Oscar Pavon <pavon_oscar@outlook.com>
2024-12-21 00:24:36 +0000

    fixed relative directories of scripts

--- scripts_names.h
+++ scripts_names.h
@@ -1,9 +1,9 @@
 #ifndef _SCRIPTS_NAMES_H_
 #define _SCRIPTS_NAMES_H_
 
-static const char * make_usb_script = "./simulate.sh";
+static const char * make_usb_script = "./make_usb.sh";
 
-static const char * get_devices_names_script = "/root/prufus/get_device_name.sh";
-static const char * get_devices_script = "/root/prufus/get_devices.sh";
-static const char * get_devices_size_script = "/root/prufus/get_sizes.sh";
+static const char * get_devices_names_script = "./get_device_name.sh";
+static const char * get_devices_script = "./get_devices.sh";
+static const char * get_devices_size_script = "./get_sizes.sh";
 #endif
