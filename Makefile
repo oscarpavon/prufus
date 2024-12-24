2d22f07f288af8697fc3389b66af440c35d7158b
Oscar Pavon <pavon_oscar@outlook.com>
2024-12-24 07:51:44 -0300

    scripts renamed

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CFLAGS := $(shell pkg-config --cflags gtk4)
 CLINKS := $(shell pkg-config --libs gtk4)
 
-prufus: main.c user_interface.o usb_validator.o
+prufus: main.c user_interface.o usb_validator.o scripts_names.h
 	cc $(CFLAGS) usb_validator.o user_interface.o main.c -o prufus $(CLINKS)
 
 user_interface.o: user_interface.c
@@ -12,8 +12,6 @@ usb_validator.o: usb_validator.c
 
 main_compile_command.o: main.c
 	cc $(CFLAGS) -c main.c
-
-
 
 compile_commands: main_compile_command.o
 	make --always-make --dry-run
--- scripts_names.h
+++ scripts_names.h
@@ -1,9 +1,9 @@
 #ifndef _SCRIPTS_NAMES_H_
 #define _SCRIPTS_NAMES_H_
 
-static const char * make_usb_script = "./make_usb.sh";
+static char * make_usb_script = "./prufus.sh";
 
-static const char * get_devices_names_script = "./get_device_name.sh";
-static const char * get_devices_script = "./get_devices.sh";
-static const char * get_devices_size_script = "./get_sizes.sh";
+static const char * get_devices_names_script = "./prufus_dev_names.sh";
+static const char * get_devices_script = "./prufus_devices.sh";
+static const char * get_devices_size_script = "./prufus_dev_sizes.sh";
 #endif
