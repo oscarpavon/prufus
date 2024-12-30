59ce5d488836e5e39f74f50410b24241fdb1ca2f
Oscar Pavon <pavon_oscar@outlook.com>
2024-12-29 23:42:42 -0300

    add a command and remove the first comma in get_usb_disks.sh

--- scripts/get_usb_disks.sh
+++ scripts/get_usb_disks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lsblk -P -d -n -o HOTPLUG,PATH,MODEL,SIZE | sed '/HOTPLUG="0"/d; s/\w*=//g; s/" /,/g; s/"//g; s/1,//g;'
+lsblk -P -d -n -o HOTPLUG,PATH,MODEL,SIZE | sed '/HOTPLUG="0"/d; s/\w*=//g; s/" /,/g; s/"//g; s/1,/,/g; s/^,\(.*\)/\1/' #add a comma and the last sed remove the first comma
