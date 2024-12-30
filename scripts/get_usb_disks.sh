89046d67cc39591fdb8f55bc8be479934e07ce1e
evjeesm <jekas.obps@gmail.com>
2024-12-30 17:57:12 +0200

    More readable and documented script

--- scripts/get_usb_disks.sh
+++ scripts/get_usb_disks.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
- 
-SED_FORMAT=\
- '/HOTPLUG="0"/d;' # filter USB drives
- 's/\w*=//g;'	   # remove keys
- 's/" /\t/g; '     # replace last \" of the values with TAB separator 
- 's/"//g;'	   # remove first \" of the values
- 's/[01]\t//g;'    # remove first HOTPLUG field
 
-lsblk -P -d -n -o HOTPLUG,PATH,MODEL,SIZE | sed ${SED_FORMAT}
+SED_FORMAT="/HOTPLUG=\"0\"/d;"         # filter USB drives
+SED_FORMAT="$SED_FORMAT s/\w*=//g;"    # remove keys
+SED_FORMAT="$SED_FORMAT s/\" /\t/g;"   # replace last \" of the values with TAB separator 
+SED_FORMAT="$SED_FORMAT s/\"//g;"      # remove first \" of the values
+SED_FORMAT="$SED_FORMAT s/^[01]\t//g;" # remove first HOTPLUG field
+
+lsblk -P -d -n -o HOTPLUG,PATH,MODEL,SIZE | sed "${SED_FORMAT}"
